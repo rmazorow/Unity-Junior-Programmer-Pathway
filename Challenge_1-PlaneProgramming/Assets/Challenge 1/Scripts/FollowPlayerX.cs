cf18121576fb0c5080316f6d5ffcb29d67f2af75
Rocky Mazorow <rmazorow@gmail.com>
2026-06-22 15:25:49 -0500

    Completed Challenge 1

---
+++ Challenge_1-PlaneProgramming/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(30, 0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Challenge_1-PlaneProgramming/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+        // Movement tuning - editable in Inspector
+    public float speed;
+    public float rotationSpeed;
+
+    // Input System action in Inspector (up/down keys)
+    public InputAction MoveAction;
+    // Current input value
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Enable the MoveAction to read key press
+        MoveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left, rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
